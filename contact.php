0dc06db0f2330ccba9fd0a2185edf8b03ed1d4bc
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-03-10 21:02:24 +0800

    wqe
    qwd

---
+++ contact.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kyōyu | Contact</title>
+
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container-fluid">
+
+      <!--<div class="backgroundimg">
+        <img src="src\index-background.jpg" alt="Image not found">
+      </div>-->
+
+      <div class="wrapper-nav">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-5">
+              <div class="weblogo">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <p>KYOYU</p>
+                  </div>
+                  <div class="col-sm-4">
+                    <p1>SINCE 2020</p1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-5">
+              <div class="navbar">
+                <li>
+                  <ul><a href="#">HOME</a></ul>
+                  <ul><a href="#">ABOUT</a></ul>
+                  <ul><a href="#">LOGIN</a></ul>
+                  <ul><a href="#">SIGN UP</a></ul>
+                </li>
+              </div>
+            </div>
+            <div class="col-sm-1"></div>
+          </div>
+        </div>
+
+        <div class="wrapper1">
+          <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-10">
+              <div class="contactintro">
+                <p>Have a word for us? We're all ears.</p>
+              </div>
+            </div>
+            <div class="col-lg-2"></div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-4">
+              <div class="contacttext">
+                <p>SUPRISE US WITH A CALL</p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="contacttext">
+                <p>+60123456789</p>
+              </div>
+            </div>
+            <div class="col-lg-2"></div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-4">
+              <div class="contacttext">
+                <p>ELECTRONIC MAILING?</p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="contacttext">
+                <p>kyoyu.ent@gmail.com</p>
+              </div>
+            </div>
+            <div class="col-lg-2"></div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+              <div class="addresslabel">
+                <p>PHYSICALLY LOCATED AT</p><br>
+              </div>
+              <div class="addresstext">
+                <p>ABC 123 ABC</p><br>
+                <p>BUKIT JALIL 57000</p><br>
+                <p>KUALA LUMPUR</p><br>
+                <p>MALAYSIA</p>
+              </div>
+            </div>
+            <div class="col-lg-2"></div>
+          </div>
+
+          <div class="kyoyutext">
+            <p>Kyōyu Since 2020</p>
+          </div>
+
+        </div>
+
+    </div><!--container-fluid-->
+  </body>
+</html>
